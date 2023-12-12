4e8350d86c0db56dd44d03c367a17f39b949ab2b
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2023-12-12 12:45:47 +0200

    Inquiry form for clients added

--- client/src/components/contactComponents/ContactForm.jsx
+++ client/src/components/contactComponents/ContactForm.jsx
@@ -1,43 +1,121 @@
+import { useEffect, useRef, useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+import useForm from '../../hooks/useForm'
+import * as inquiryService from '../../services/inquiryService'
+
 import '../../assets/styles/contact.css'
 
+const InquiryFormKeys ={
+  'Name': 'name',
+  'Email': 'email',
+  'Message': 'message'
+} 
+
 export const ContactForm = () =>{
+  const navigate = useNavigate();
+  const nameInputRef = useRef(null);
+  useEffect(()=>{
+    nameInputRef.current.focus()
+  }, []);
+
+  const inquirySubmitHandler = async (values) =>{
+    await inquiryService.create({'From': values[InquiryFormKeys.Name], 'Email': values[InquiryFormKeys.Email], 'Message': values[InquiryFormKeys.Message]});
+    navigate('/');
+  }
+
+  const {values, onChange, onSubmit} = useForm(inquirySubmitHandler, {
+    [InquiryFormKeys.Name] : '',
+    [InquiryFormKeys.Email] : '',
+    [InquiryFormKeys.Message] : ''
+  })
+
+  const [inquiryFormHasErrors, setInquiryFormHasErrors] = useState({
+    [InquiryFormKeys.Name] : '',
+    [InquiryFormKeys.Email] : '',
+    [InquiryFormKeys.Message] : ''
+  })
+  const emailErrorHandler = (e) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setInquiryFormHasErrors((state) => ({
+      ...state,
+      [e.target.name]: !emailRegex.test(e.target.value),
+    }));
+  };
+
+  const nameAndMessageErrorHandler = (e, minLength, maxLength) => {
+    setInquiryFormHasErrors((state) => ({
+      ...state,
+      [e.target.name]:
+        values[e.target.name].length < minLength ||
+        values[e.target.name].length > maxLength,
+    }));
+  };
   
     return(
         <div className="col-lg-8 contact_col">
         <div className="contact_title">Get in touch</div>
         <div className="contact_form_container">
-          <form action="#" id="contact_form" className="contact_form">
+          <form onSubmit={onSubmit} id="contact_form" className="contact_form">
             <div className="row">
               <div className="col-lg-6">
                 <div className="input_item">
+                {inquiryFormHasErrors[InquiryFormKeys.Name] && (
+                        <span className="error_message">
+                          Please enter valid name between 2 and 20 characters
+                        </span>
+                      )} 
                   <input
                     type="text"
                     className="contact_input trans_200"
-                    placeholder="Name"
+                    placeholder="Name - between 2 and 20 characters"
                     required="required"
+                    ref={nameInputRef}
+                    name={InquiryFormKeys.Name}
+                    value={values[InquiryFormKeys.Name]}
+                    onChange={onChange}
+                    onBlur={(e) => nameAndMessageErrorHandler(e, 2, 20)}
                   />
                 </div>
               </div>
               <div className="col-lg-6">
                 <div className="input_item">
+                {inquiryFormHasErrors[InquiryFormKeys.Email] && (
+                        <span className="error_message">
+                          In order to receive our answer, please type correct e-mail address.
+                        </span>
+                      )} 
                   <input
                     type="email"
                     className="contact_input trans_200"
                     placeholder="E-mail"
                     required="required"
+                    name={InquiryFormKeys.Email}
+                    value={values[InquiryFormKeys.Email]}
+                    onChange={onChange}
+                    onBlur={(e) => emailErrorHandler(e)}
                   />
                 </div>
               </div>
             </div>
             <div className="input_item">
+            {inquiryFormHasErrors[InquiryFormKeys.Message] && (
+                        <span className="error_message">
+                          Please enter valid message between 10 and 1000 characters
+                        </span>
+                      )}
               <textarea
                 className="contact_input contact_textarea trans_200"
-                placeholder="Message"
+                placeholder="Message - between 10 and 1000 characters"
                 required="required"
-                defaultValue={""}
+                name={InquiryFormKeys.Message}
+                value={values[InquiryFormKeys.Message]}
+                onChange={onChange}
+                onBlur={(e) => nameAndMessageErrorHandler(e, 10, 1000)}
               />
             </div>
-            <button className="contact_button button">
+            <button type="submit" className="contact_button button" disabled={Object.values(inquiryFormHasErrors).some(
+                      (x) => x
+                    )}>
               Send
               <span />
             </button>
